dc91ed1038b973a0ab599db20e4ce63e99993736
Kizmit <56753837+Kizmit@users.noreply.github.com>
2020-07-26 19:56:58 -0500

    More UI additions
    Added AI vs AI difficulty select/game start menu
    Added player vs AI difficulty select/game start menu
    Created empty AI vs AI game scene
    Organized gameplay/menu scripts into folders
    Tested actual build - works as intended

---
+++ UnityBattleshipAI/Assets/Scripts/Menu Scripts/NewGameMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewGameMenu : MonoBehaviour
+{
+  public void NewGame() //New Game button
+   {
+       Debug.Log("New Game clicked; loading next scene in build index");
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //loads the next scene in the build index on click
+   }
+}
