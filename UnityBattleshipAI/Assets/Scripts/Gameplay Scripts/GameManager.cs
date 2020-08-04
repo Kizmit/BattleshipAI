aacb3a175fa283385c58fb5f20a706df5a85782b
sjm225 <sjm225@txstate.edu>
2020-08-04 13:12:57 -0500

    Update GameManager.cs
    Continuing work on AI algorithms
    
    (if I don't push the changes before switching branches, ithub doesn't let me switch branches back...)

--- UnityBattleshipAI/Assets/Scripts/Gameplay Scripts/GameManager.cs
+++ UnityBattleshipAI/Assets/Scripts/Gameplay Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using System.Linq;
 
 //This script manages the enemy AI behavior
 public class GameManager : MonoBehaviour, IComparer
@@ -235,9 +236,10 @@ public class GameManager : MonoBehaviour, IComparer
             }
         }
     }
-
+/********************************** ALGORITHMS ***********************************************/
     public void AIPureRNG()
     {
+        /* Algorithm: Randomly searches the board for hits. */
         bool foundIndex = false;
         int index;
         while(!foundIndex)
@@ -255,21 +257,108 @@ public class GameManager : MonoBehaviour, IComparer
 
     private void AIMedium()
     {
+        /* Algorithm: Randomly searches the board until it hits a ship.
+         * When a ship is hit, the algorithm will check three spaces above the original hit, three below, 
+         * three to the left, and three to the right. 
+         * After, the algorithm continues randomly searching for another hit.
+        */
         int index;
+        bool hit;
         index = RandomNumberGenerator(GRID_SIZE);
-        CheckAIHit(playerGridCells[index]);
+        hit = CheckAIHit(playerGridCells[index]);
+        int newIndex = index;
+
+        // Check that the new index has not been previously called.
+        // Check that the new index is in-bounds by rows (0-9, 10-19, 20-29, ..., 90-99).
+        // Check the the new index is in-bounds by columns (??)
+        // NOTE: What would happen if we didn't check bounds? Obviously, it would be slightly less efficient
+        // (such as continuing on to the next row, even though it's not possible for the ship to wrap to the next row).
+        // Ignoring NOTE for now.
+
+        // ABOVE: up one space (index - 10), up two spaces (index - 20), up three spaces (index - 30)
+        while ((hit) && (newIndex <= (index - 30))) // While there is a hit and we are no more than three spaces away from the original hit...
+        {
+            newIndex -= 10;
+            if (inBoundsRow(index, newIndex) || inBoundsCol(index, newIndex))
+            {
+                hit = CheckAIHit(playerGridCells[newIndex]);
+            }
+            else
+            {
+                hit = false;
+            }
+        }
+
+        // BELOW: down one (index + 10), down two (index + 20), down three (index + 30)
+        newIndex = index;
+        while ((hit) && (newIndex <= (index + 30)))
+        {
+            newIndex += 10;
+            if (inBoundsRow(index, newIndex) || inBoundsCol(index, newIndex))
+            {
+                hit = CheckAIHit(playerGridCells[newIndex]);
+            }
+            else
+            {
+                hit = false;
+            }
+        }
+
+        // LEFT: (index - 1), (index - 2), (index -3)
+        newIndex = index;
+        while ((hit) && (newIndex <= (index - 3)))
+        {
+            newIndex -= 1;
+            if (inBoundsRow(index, newIndex) || inBoundsCol(index, newIndex))
+            {
+                hit = CheckAIHit(playerGridCells[newIndex]);
+            }
+            else
+            {
+                hit = false;
+            }
+        }
+
+        // RIGHT: (index + 1), (index + 2), (index + 3)
+        newIndex = index;
+        while ((hit) && (newIndex <= (index + 3)))
+        {
+            newIndex += 1;
+            if (inBoundsRow(index, newIndex) || inBoundsCol(index, newIndex))
+            {
+                hit = CheckAIHit(playerGridCells[newIndex]);
+            }
+            else
+            {
+                hit = false;
+            }
+        }
     }
 
     private void AIHard()
     {
-
+        /* Algorithm: Starts at index 0 and searches every other space of the board (like only checking 
+         * the black spaces of a checkerboard) until a ship is hit.
+         * When a ship is hit, the algorithm will check three spaces above the original hit, three below, 
+         * three to the left, and three to the right. 
+         * After, the algorithm continues searching every other space (from where it left off) until
+         * another ship is hit.
+        */
+        int index;
+        bool hit;
+        index = RandomNumberGenerator(GRID_SIZE);
+        hit = CheckAIHit(playerGridCells[index]);
+        int newIndex = index;
     }
 
     private void AIImpossible()
     {
+        /* Algorithm: The enemyAI already knows every location of the player's ships and gets a hit every turn. */
         CheckAIHit(playerShipLocations[impossibleIndex]);
         impossibleIndex++;
     }
+
+/*********************************************************************************************/
 
     private int RandomNumberGenerator(int bound) //random number generator for placing ships
     {
@@ -295,18 +384,21 @@ public class GameManager : MonoBehaviour, IComparer
         GetComponent<InputManager>().SetPlayerTurn();
     }
 
-    public void CheckAIHit(GameObject cell)
+    public bool CheckAIHit(GameObject cell)
     {
         if(playerShipLocations.Contains(cell.gameObject)) //hit
         {
             cell.GetComponent<GridChanges>().ChangeSpriteRed();
             totalAIHits++;
+            GetComponent<InputManager>().SetPlayerTurn();
+            return true;
         } 
         else //miss
         {
-            cell.GetComponent<GridChanges>().ChangeSpriteWhite(); 
+            cell.GetComponent<GridChanges>().ChangeSpriteWhite();
+            GetComponent<InputManager>().SetPlayerTurn();
+            return false;
         }
-        GetComponent<InputManager>().SetPlayerTurn();
     }
 
     public void SetCoordinatesOfShip()
@@ -349,6 +441,132 @@ public class GameManager : MonoBehaviour, IComparer
             if(RandomNumberGenerator(2) == 1) GetComponent<InputManager>().SetPlayerTurn();
             gameRunning = true;
 
+        }
+    }
+
+    // Takes the original index and newIndex and checks if they are in the same row.
+    public bool inBoundsRow(int i, int ni)
+    {
+        int index = i;
+        int newIndex = ni;
+
+        int[] row1 = { 0, 1, 2, 9, 4, 5, 6, 7, 8, 9 };
+        int[] row2 = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+        int[] row3 = { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
+        int[] row4 = { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39 };
+        int[] row5 = { 40, 41, 42, 43, 44, 45, 46, 47, 48, 49 };
+        int[] row6 = { 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 };
+        int[] row7 = { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69 };
+        int[] row8 = { 70, 71, 72, 73, 74, 75, 76, 77, 78, 79 };
+        int[] row9 = { 80, 81, 82, 83, 84, 85, 86, 87, 88, 89 };
+        int[] row10 = { 90, 91, 92, 93, 94, 95, 96, 97, 98, 99 };
+
+        if (row1.Contains(index) && row1.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row2.Contains(index) && row2.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row3.Contains(index) && row3.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row4.Contains(index) && row4.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row5.Contains(index) && row5.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row6.Contains(index) && row6.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row7.Contains(index) && row7.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row8.Contains(index) && row8.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row9.Contains(index) && row9.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (row10.Contains(index) && row10.Contains(newIndex))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Takes the original index and newIndex and checks if they are in the same column.
+    public bool inBoundsCol(int i, int ni)
+    {
+        int index = i;
+        int newIndex = ni;
+
+        int[] col1 = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+        int[] col2 = { 1, 11, 21, 31, 41, 51, 61, 71, 81, 91 };
+        int[] col3 = { 2, 12, 22, 32, 42, 52, 62, 72, 82, 92 };
+        int[] col4 = { 3, 13, 23, 33, 43, 53, 63, 73, 83, 93 };
+        int[] col5 = { 4, 14, 24, 34, 44, 54, 64, 74, 84, 94 };
+        int[] col6 = { 5, 15, 25, 35, 45, 55, 65, 75, 85, 95 };
+        int[] col7 = { 6, 16, 26, 36, 46, 56, 66, 76, 86, 96 };
+        int[] col8 = { 7, 17, 27, 37, 47, 57, 67, 77, 87, 97 };
+        int[] col9 = { 8, 18, 28, 38, 48, 58, 68, 78, 88, 98 };
+        int[] col10 = { 9, 19, 29, 39, 49, 59, 69, 79, 89, 99 };
+
+        if (col1.Contains(index) && col1.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col2.Contains(index) && col2.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col3.Contains(index) && col3.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col4.Contains(index) && col4.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col5.Contains(index) && col5.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col6.Contains(index) && col6.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col7.Contains(index) && col7.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col8.Contains(index) && col8.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col9.Contains(index) && col9.Contains(newIndex))
+        {
+            return true;
+        }
+        else if (col10.Contains(index) && col10.Contains(newIndex))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
