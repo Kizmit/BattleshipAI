b416aa95478fe73ea67fe1a485200e96463fe189
sjm225 <sjm225@txstate.edu>
2020-08-03 21:26:43 -0500

    Trying to fix AIMedium
    Most recent progress:
    - still working on AIMedium
    - started to implement a function (called inBounds) to check if an index (specifically, a new index generated by the algorithm) is on the same row and/or on the same column as the original index

--- UnityBattleshipAI/Assets/Scripts/Gameplay Scripts/GameManager.cs
+++ UnityBattleshipAI/Assets/Scripts/Gameplay Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using System.Linq;
 
 //This script manages the enemy AI behavior
 public class GameManager : MonoBehaviour, IComparer
@@ -254,15 +255,13 @@ public class GameManager : MonoBehaviour, IComparer
          * When a ship is hit, the algorithm will check three spaces above the original hit, three below, 
          * three to the left, and three to the right. 
          * After, the algorithm continues randomly searching for another hit.
-        */
+        */    
         int index;
         bool hit;
-        
         index = RandomNumberGenerator(GRID_SIZE);
         hit = CheckAIHit(playerGridCells[index]);
+        int newIndex = index;
 
-        if (hit)
-        {
             // Check that the new index has not been previously called.
             // Check that the new index is in-bounds by rows (0-9, 10-19, 20-29, ..., 90-99).
             // Check the the new index is in-bounds by columns (??)
@@ -271,10 +270,9 @@ public class GameManager : MonoBehaviour, IComparer
             // Ignoring NOTE for now.
 
             // ABOVE: up one space (index - 10), up two spaces (index - 20), up three spaces (index - 30)
-            int newIndex = index;
             while ((hit) && (newIndex <= (index - 30))) // While there is a hit and we are no more than three spaces away from the original hit...
             {
-                newIndex = index - 10;
+                newIndex -= 10;
                 // Check that newIndex is in bounds (implement later) and has not been previously attacked.
                 hit = CheckAIHit(playerGridCells[newIndex]);
             }
@@ -283,7 +281,7 @@ public class GameManager : MonoBehaviour, IComparer
             newIndex = index;
             while ((hit) && (newIndex <= (index + 30)))
             {
-                newIndex = index + 10;
+                newIndex += 10;
                 // Check that newIndex is in bounds (implement later) and has not been previously attacked.
                 hit = CheckAIHit(playerGridCells[newIndex]);
             }
@@ -292,7 +290,7 @@ public class GameManager : MonoBehaviour, IComparer
             newIndex = index;
             while ((hit) && (newIndex <= (index - 3)))
             {
-                newIndex = index - 10;
+                newIndex -= 1;
                 // Check that newIndex is in bounds (implement later) and has not been previously attacked.
                 hit = CheckAIHit(playerGridCells[newIndex]);
             }
@@ -301,13 +299,10 @@ public class GameManager : MonoBehaviour, IComparer
             newIndex = index;
             while ((hit) && (newIndex <= (index + 3)))
             {
-                newIndex = index + 1;
+                newIndex += 1;
                 // Check that newIndex is in bounds (implement later) and has not been previously attacked.
                 hit = CheckAIHit(playerGridCells[newIndex]);
             }
-
-        }
-
     }
 
     private void AIHard()
@@ -428,5 +423,42 @@ public class GameManager : MonoBehaviour, IComparer
         SetCoordinatesOfShip();
         if(RandomNumberGenerator(2) == 1) GetComponent<InputManager>().SetPlayerTurn();
         gameRunning = true;
+    }
+
+    public bool inBounds(int i, int ni)
+    {
+        int index = i;
+        int newIndex = ni;
+
+        int[] row1 = { 0, 1, 2, 9, 4, 5, 6, 7, 8, 9 };
+        int[] row2 = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 }; 
+        int[] row3 = { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
+        int[] row4 = { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39 }; 
+        int[] row5 = { 40, 41, 42, 43, 44, 45, 46, 47, 48, 49 }; 
+        int[] row6 = { 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 }; 
+        int[] row7 = { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69 }; 
+        int[] row8 = { 70, 71, 72, 73, 74, 75, 76, 77, 78, 79 }; 
+        int[] row9 = { 80, 81, 82, 83, 84, 85, 86, 87, 88, 89 }; 
+        int[] row10 = { 90, 91, 92, 93, 94, 95, 96, 97, 98, 99 };
+
+        int[] col1 = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+        int[] col2 = { 1, 11, 21, 31, 41, 51, 61, 71, 81, 91 };
+        int[] col3 = { 2, 12, 22, 32, 42, 52, 62, 72, 82, 92 };
+        int[] col4 = { 3, 13, 23, 33, 43, 53, 63, 73, 83, 93 };
+        int[] col5 = { 4, 14, 24, 34, 44, 54, 64, 74, 84, 94 };
+        int[] col6 = { 5, 15, 25, 35, 45, 55, 65, 75, 85, 95 };
+        int[] col7 = { 6, 16, 26, 36, 46, 56, 66, 76, 86, 96 };
+        int[] col8 = { 7, 17, 27, 37, 47, 57, 67, 77, 87, 97 };
+        int[] col9 = { 8, 18, 28, 38, 48, 58, 68, 78, 88, 98 };
+        int[] col10 = { 9, 19, 29, 39, 49, 59, 69, 79, 89, 99 };
+
+        if (row1.Contains(index) && row1.Contains(newIndex))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
